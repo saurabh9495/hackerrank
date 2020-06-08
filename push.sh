49554a5a18ce91b093c22f89644fe29ceb55565f
saurabh9495 <saurabhkumar4829@gmail.com>
2020-06-09 00:30:42 +0530

    Coding Commits made on 8th June 2020 Problems solved on hacekrrank.

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "Coding Commits made on 7th June 2020 Problems solved on hacekrrank."
+git commit -m "Coding Commits made on 8th June 2020 Problems solved on hacekrrank."
 git push origin master
